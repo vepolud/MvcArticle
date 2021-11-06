c4728f0732c48b03a10960df4938d5cfbd1d51c3
vepolud <vepolud@gmail.com>
2021-11-06 22:19:58 +0300

    Init

---
+++ src/main/java/ru/javaschool/polud/mvcarticle/MvcarticleApplication.java
@@ -0,0 +1,13 @@
+package ru.javaschool.polud.mvcarticle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcarticleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MvcarticleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/javaschool/polud/mvcarticle/MvcarticleApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.javaschool.polud.mvcarticle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcarticleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
