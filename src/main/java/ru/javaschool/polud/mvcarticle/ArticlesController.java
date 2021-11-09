5a3d200df3d723ef475faaea6556db22c444a987
vepolud <vepolud@gmail.com>
2021-11-10 00:21:45 +0300

    HAVE DONE data validation and upload form has done

--- src/main/java/ru/javaschool/polud/mvcarticle/ArticlesController.java
+++ src/main/java/ru/javaschool/polud/mvcarticle/ArticlesController.java
@@ -1,14 +1,12 @@
 package ru.javaschool.polud.mvcarticle;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.Map;
-import java.util.Optional;
 
 @Controller
 public class ArticlesController {
@@ -21,37 +19,35 @@ public class ArticlesController {
     }
 
     @GetMapping("/articles")
-    public String getArticleList(
-            /*@RequestParam(name="name", required=false, defaultValue="No articles") String name,*/
-            Map<String, Object> model) {
-        Iterable <Article> articles = articleRepository.findAll();
+    public String getArticleList(Map<String, Object> model) {
+        Iterable<Article> articles = articleRepository.findAll();
         model.put("articles", articles);
         return "articles";
     }
 
-    @GetMapping("/upload")
-    public String greeting() {
-
-        return "upload";
-    }
+//    @GetMapping("/upload")
+//    public String greeting() {
+//
+//        return "upload";
+//    }
 
     @GetMapping("/article")
-    public String getArticle(
-            @RequestParam(name = "id", required=false, defaultValue="1") String id,
-            Model model) {
-        System.out.println(id);
-        Article article = articleRepository.findById(Long.valueOf(id)).orElse(null) ;
+    public String newArticle(
+//            @RequestParam(name = "id", required = false, defaultValue = "1") String id,
+            Map<String, String> model) {
 
-        model.addAttribute("article", article);
+//        Article article = articleRepository.findById(Long.valueOf(id)).orElse(null);
+
+//        model.addAttribute("article", article);
+        model.put("message", "Please use form to upload your article");
         return "article";
     }
 
 
-
-
     @PostMapping("/article")
-    public String newArticle(@RequestParam MultipartFile file) {
-        unzipService.getArticle(file);
-        return "redirect:/articles";
+    public String uploadArticle(@RequestParam MultipartFile file, Map<String, String> model) {
+        String message = unzipService.getArticle(file);
+        model.put("message", message);
+        return "/article";
     }
 }
--- src/main/java/ru/javaschool/polud/mvcarticle/UnzipService.java
+++ src/main/java/ru/javaschool/polud/mvcarticle/UnzipService.java
@@ -1,12 +1,11 @@
 package ru.javaschool.polud.mvcarticle;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
 @Component
@@ -18,24 +17,28 @@ public class UnzipService {
         this.articleRepository = articleRepository;
     }
 
-    public Article getArticle(MultipartFile file) {
-
-//        Article article = new Article();
-        try {
-            ZipInputStream zipInputStream = new ZipInputStream(file.getInputStream());
-            while (zipInputStream.getNextEntry() != null){
+    public String getArticle(MultipartFile file) {
+        if (file.isEmpty() || !file.getContentType().equals("application/x-zip-compressed")) {
+            return "Uploaded file is not a zip archive";
+        }
+        try (ZipInputStream zipInputStream = new ZipInputStream(file.getInputStream())) {
+            ZipEntry zipEntry = zipInputStream.getNextEntry();
+            if (zipEntry != null && zipEntry.getName().equals("article.TXT")) {
                 Scanner scanner = new Scanner(zipInputStream);
                 String header = scanner.nextLine();
+                if (!scanner.hasNext()) {
+                    return "There is only one line in the file";
+                }
                 StringBuilder sb = new StringBuilder();
                 while (scanner.hasNext()) {
                     sb.append(scanner.nextLine());
-//                    System.out.println(scanner.nextLine());
+                }
+                if (zipInputStream.getNextEntry() != null) {
+                    return "There are more then one files in the zip archive";
                 }
                 articleRepository.save(new Article(header, sb.toString()));
-                System.out.println(header);
-                System.out.println(sb);
-
-            }
+                return "The file was successfully uploaded";
+            } else return "There are no article.TXT in uploaded zip archive";
         } catch (IOException e) {
             e.printStackTrace();
         }
